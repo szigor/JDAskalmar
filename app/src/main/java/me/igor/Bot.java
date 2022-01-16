c7cb00a07075fc553585f4e7418e8dcdf425afa8
szigor <93654954+szigor@users.noreply.github.com>
2022-01-16 17:06:03 +0100

    9th

--- app/src/main/java/me/igor/Bot.java
+++ app/src/main/java/me/igor/Bot.java
@@ -2,6 +2,7 @@ package me.igor;
 
 import me.igor.Commands.commands.HelpCommand;
 import me.igor.Commands.commands.PingCommand;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -12,7 +13,7 @@ import javax.security.auth.login.LoginException;
 public class Bot {
     private Bot() throws LoginException {
 
-        JDABuilder.createDefault(
+        JDA jda JDABuilder.createDefault(
                         Config.get("token"),
                         GatewayIntent.GUILD_MESSAGES,
                         GatewayIntent.GUILD_VOICE_STATES
@@ -22,7 +23,7 @@ public class Bot {
                         new Listener(),
                         new HelpCommand(),
                         new PingCommand())
-                //.setActivity(Activity.listening("dj!help - komendy"))
+                .setActivity(Activity.listening("dj!help - komendy"))
                 .build();
     }
     public static void main(String[] args) throws LoginException {
